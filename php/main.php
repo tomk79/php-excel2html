032dffc67260e4113243633087c20d93f85a852b
Tomoya Koyanagi <tomk79@gmail.com>
2015-06-08 18:49:25 +0900

    オプション strip_table_tag, header_row, header_col を実行

--- php/main.php
+++ php/main.php
@@ -29,27 +29,73 @@ class main{
 
 	/**
 	 * get converted html
+	 * @param array $options オプション
+	 * <dl>
+	 *   <dt>string renderer</dt>
+	 *     <dd>レンダリングモード。<code>simplify</code>(単純化)、または<code>strict</code>(そのまま表示) のいずれかを指定します。デフォルトは <code>strict</code> です。</dd>
+	 *   <dt>string cell_renderer</dt>
+	 *     <dd>セルのレンダリングモード。<code>html</code>(HTMLコードとして処理)、<code>text</code>(プレーンテキストとして処理)、または<code>markdown</code>(Markdownとして処理) のいずれかを指定します。デフォルトは <code>text</code> です。</dd>
+	 *   <dt>int header_row</dt>
+	 *     <dd>ヘッダー行の番号。デフォルトは 0。</dd>
+	 *   <dt>int header_col</dt>
+	 *     <dd>ヘッダー列の番号。デフォルトは 0。</dd>
+	 *   <dt>bool strip_table_tag</dt>
+	 *     <dd>tableタグを削除するかどうか。true のとき、tableタグは削除した状態で出力されます。デフォルトは false です。</dd>
+	 * </dl>
 	 */
-	public function get_html(){
+	public function get_html($options=array()){
+		$options['header_row'] = @intval($options['header_row']);
+		$options['header_col'] = @intval($options['header_col']);
 
 		$objWorksheet = $this->objPHPExcel->getActiveSheet();
 
 		$rtn = '';
-		$rtn .= '<table>' . PHP_EOL;
-		foreach ($objWorksheet->getRowIterator() as $row) {
-			$rtn .= '<tr>' . PHP_EOL;
+		if( !@$options['strip_table_tag'] ){
+			$rtn .= '<table>'.PHP_EOL;
+		}
+		$thead = '';
+		$tbody = '';
+		foreach ($objWorksheet->getRowIterator() as $rowIdx=>$row) {
+			// var_dump($rowIdx); //← $rowIdx は1から始まります
+
+			$tmpRow = '';
+			$tmpRow .= '<tr>'.PHP_EOL;
 			$cellIterator = $row->getCellIterator();
 			$cellIterator->setIterateOnlyExistingCells(true);
 				// This loops through all cells,
 				//    even if a cell value is not set.
 				// By default, only cells that have a value 
 				//    set will be iterated.
-			foreach ($cellIterator as $cell) {
-				$rtn .= '<td>' . $cell->getValue() . '</td>' . PHP_EOL;
+			foreach ($cellIterator as $colIdxName=>$cell) {
+				$colIdx = \PHPExcel_Cell::columnIndexFromString( $colIdxName );
+				// var_dump($colIdx); //← $colIdx は1から始まります
+				$cellTagName = 'td';
+				if( $rowIdx <= $options['header_row'] || $colIdx <= $options['header_col'] ){
+					$cellTagName = 'th';
+				}
+				$tmpRow .= '<'.$cellTagName.'>'.$cell->getCalculatedValue().'</'.$cellTagName.'>'.PHP_EOL;
 			}
-			$rtn .= '</tr>' . PHP_EOL;
+			$tmpRow .= '</tr>'.PHP_EOL;
+
+			if( $rowIdx <= $options['header_row'] ){
+				$thead .= $tmpRow;
+			}else{
+				$tbody .= $tmpRow;
+			}
 		}
-		$rtn .= '</table>' . PHP_EOL;
+
+		if( strlen($thead) ){
+			$rtn .= '<thead>'.PHP_EOL;
+			$rtn .= $thead;
+			$rtn .= '</thead>'.PHP_EOL;
+		}
+		$rtn .= '<tbody>'.PHP_EOL;
+		$rtn .= $tbody;
+		$rtn .= '</tbody>'.PHP_EOL;
+
+		if( !@$options['strip_table_tag'] ){
+			$rtn .= '</table>'.PHP_EOL;
+		}
 
 		return $rtn;
 	}
--- tests/mainTest.php
+++ tests/mainTest.php
@@ -36,8 +36,39 @@ class mainTest extends PHPUnit_Framework_TestCase{
 			DEFAULT_SPAN_TEXT // $defaultSpanText
 		);
 
+		$this->assertEquals( 1, count($html->find('table')) );
+		$this->assertEquals( 0, count($html->find('thead')) );
 		$this->assertEquals( 18, count($html->find('tr')) );
 		$this->assertEquals( 5, count($html->find('tr',0)->find('td')) );
+		$this->assertEquals( 'E18', $html->find('tr',17)->childNodes(4)->innertext );
+		$this->assertNull( $html->find('tr',18) );
+		$this->assertNull( $html->find('tr',17)->childNodes(5) );
+
+
+
+		$src = (new \tomk79\excel2html\main(__DIR__.'/sample/default.xlsx'))->get_html(array(
+			'header_row'=>2,
+			'header_col'=>1,
+			'strip_table_tag'=>true
+		));
+		// var_dump($src);
+
+		$html = str_get_html(
+			$src ,
+			true, // $lowercase
+			true, // $forceTagsClosed
+			DEFAULT_TARGET_CHARSET, // $target_charset
+			false, // $stripRN
+			DEFAULT_BR_TEXT, // $defaultBRText
+			DEFAULT_SPAN_TEXT // $defaultSpanText
+		);
+
+		$this->assertEquals( 0, count($html->find('table')) );
+		$this->assertEquals( 1, count($html->find('thead')) );
+		$this->assertEquals( 2, count($html->find('thead tr')) );
+		$this->assertEquals( 18, count($html->find('tr')) );
+		$this->assertEquals( 5, count($html->find('tr',0)->find('th')) );
+		$this->assertEquals( 4, count($html->find('tr',2)->find('td')) );
 		$this->assertEquals( 'E18', $html->find('tr',17)->childNodes(4)->innertext );
 		$this->assertNull( $html->find('tr',18) );
 		$this->assertNull( $html->find('tr',17)->childNodes(5) );
